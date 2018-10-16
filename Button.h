6c65e3a08e0dc05c63fdce5bc675d81194f6ad77
Martin <xboxpro1@gmail.com>
2018-10-16 06:40:50 +0200

    Update Button.h

--- Button.h
+++ Button.h
@@ -10,15 +10,15 @@
 #define Button_h
 
 #define DEFAULT_DEBOUNCE_DELAY 80
-#define DEFAULT_STATE HIGH
+#define DEFAULT_MODE INPUT_PULLUP
 
 
 class Button
 {
 	public:
-		explicit Button(int pin = -1, bool state = DEFAULT_STATE, int delay = DEFAULT_DEBOUNCE_DELAY, bool has_changed = false, unsigned long ignore_until = 0);
+		explicit Button(int pin = -1, int mode = DEFAULT_MODE, int delay = DEFAULT_DEBOUNCE_DELAY, bool has_changed = false, unsigned long ignore_until = 0);
 		virtual ~Button();
-		void setup(int pin, bool state = DEFAULT_STATE, int delay = DEFAULT_DEBOUNCE_DELAY, bool has_changed = false, unsigned long ignore_until = 0);
+		void setup(int pin, int mode = DEFAULT_MODE, int delay = DEFAULT_DEBOUNCE_DELAY, bool has_changed = false, unsigned long ignore_until = 0);
 		bool read();
 		bool toggled();
 		bool pressed();
@@ -28,6 +28,7 @@ class Button
 			
 	private:
 		int	_pin;
+		int	_mode;
 		int	_delay;
 		bool	_state;
 		bool	_defaultstate;
